7ae1651f63ecfaf74aeb185fa2862d1a31fa9e53
Tommaso <tbianchi866@gmail.com>
2022-06-03 16:35:38 +0200

    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ProgAngular';
+export class AppComponent
+{
+  data:any =
+  [
+    {id: 10001,birthDate: "1953-09-01",firstName: "Georgi",lastName: "Facello",gender: "M",hireDate: "1986-06-25"},
+    {id: 10002,birthDate: "1964-06-01",firstName: "Bezalel",lastName: "Simmel",gender: "F",hireDate: "1985-11-20"},
+    {id: 10003,birthDate: "1959-12-02",firstName: "Parto",lastName: "Bamford",gender: "M",hireDate: "1986-08-27T22:00:00.000+0000"},
+    {id: 10004,birthDate: "1954-04-30",firstName: "Chirstian",lastName: "Koblick",gender: "M",hireDate: "1986-11-30"},
+    {id: 10005,birthDate: "1955-01-20",firstName: "Kyoichi",lastName: "Maliniak",gender: "M",hireDate: "1989-09-11T22:00:00.000+0000"}
+  ];
+
+  constructor()
+  {}
+
+  Aggiungi():void
+  {
+
+  }
+
+  Elimina(employee:any):void
+  {
+    let id = employee.id;
+
+    for(let i = 0; i < this.data.length; i++)
+    {
+      if(id == this.data.id)
+      {
+        this.data.splice(i,1);
+      }
+    }
+  }
 }
